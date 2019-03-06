f8563480a3ec5015b9cfeef378d7eb8506bb79b2
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-06 10:36:20 -0300

    Create aula05DB.sql

---
+++ aula05DB.sql
@@ -0,0 +1,6 @@
+insert into pessoas values
+(default, 'Marcos', '2004-07-06', 'M', '45.5', '1.58', 'Irlanda'),
+(default, 'Max', '2006-08-06','M', '51', '1.64', default),
+(default, 'Carlos', '2002-04-06','M', '55.7', '1.70', 'África do Sul');
+
+select * from pessoas;
