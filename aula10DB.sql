db179b9e7d23f44c8d44714d4c0430b5d71b430a
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-11 13:04:55 -0300

    Create aula10DB.sql

---
+++ aula10DB.sql
@@ -0,0 +1,7 @@
+show databases;                -- > MOSTRA OS BANCOS DE DADOS CRIADOS
+use exemplo;                   -- > SELECIONA UM DB PARA SER USADO
+show tables;                   -- > MOSTRA TABELAS DO DB
+desc amigos;                   -- > DESCREVE TABELA SELECIONADA
+show create table amigos;      -- > MOSTRA COMANDO USADO PARA CRIAR A TABELA SELECIONADA
+show create database exemplo;  -- > MOSTRA COMANDO USADO PARA CRIAR DB SELECIONADO
+select * from amigos;          -- > MOSTRA DADOS DA TABELA AMIGOS
